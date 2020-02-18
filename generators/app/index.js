c8bb27defe3dcff981d8490c8a80ea28471ec08c
afeiship <1290657123@qq.com>
2020-02-18 19:30:45 +0800

    add: update compose with stdapp

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,35 +1,39 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const glob = require('glob');
-const { resolve } = require('path');
-const remote = require('yeoman-remote');
-const yoHelper = require('@feizheng/yeoman-generator-helper');
-const replace = require('replace-in-file');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const glob = require("glob");
+const { resolve } = require("path");
+const remote = require("yeoman-remote");
+const yoHelper = require("@feizheng/yeoman-generator-helper");
+const replace = require("replace-in-file");
 
 module.exports = class extends Generator {
+  initializing() {
+    this.composeWith("dotfiles:stdapp");
+  }
+
   prompting() {
     // Have Yeoman greet the user.
     this.log(
       yosay(
         `Welcome to the stunning ${chalk.red(
-          'generator-boilerplate-generator'
+          "generator-boilerplate-generator"
         )} generator!`
       )
     );
 
     const prompts = [
       {
-        type: 'input',
-        name: 'project_name',
-        message: 'Your project_name (eg: like this `react-button` )?',
+        type: "input",
+        name: "project_name",
+        message: "Your project_name (eg: like this `react-button` )?",
         default: yoHelper.discoverRoot
       },
       {
-        type: 'input',
-        name: 'description',
-        message: 'Your description?'
+        type: "input",
+        name: "description",
+        message: "Your description?"
       }
     ];
 
@@ -40,6 +44,10 @@ module.exports = class extends Generator {
         yoHelper.rewriteProps(props);
       }.bind(this)
     );
+  }
+
+  install() {
+    this.npmInstall();
   }
 
   writing() {
@@ -60,7 +68,7 @@ module.exports = class extends Generator {
 
   end() {
     const { project_name, description, ProjectName } = this.props;
-    const files = glob.sync(resolve(this.destinationPath(), '{**,.*}'));
+    const files = glob.sync(resolve(this.destinationPath(), "{**,.*}"));
 
     replace.sync({
       files,
